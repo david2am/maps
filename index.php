66037b3b91917a08a13664401be132e1b7c131b3
David Cueter <dvccueter@gmail.com>
2018-04-09 04:10:35 -0500

    Se usa 'places' para ver cafeterias cercanas en Medellin

--- index.php
+++ index.php
@@ -23,8 +23,48 @@
         }
     </style>
     <script>
-    
-    </script>
+
+        var map;
+        var infowindow;
+
+        function initMap() {
+            var medellin = {lat: 6.25184, lng: -75.56359};
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: medellin,
+                zoom: 15
+            });
+
+            infowindow = new google.maps.InfoWindow();
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: medellin,
+                radius: 500,
+                type: ['cafe']
+            }, callback);
+        }
+
+        function callback(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
+                }
+            }
+        }
+
+        function createMarker(place) {
+            var placeLoc = place.geometry.location;
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(place.name);
+                infowindow.open(map, this);
+            });
+        }
+        </script>
 </head>
 <body>
     <?php 
